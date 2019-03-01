996570adf0a8a127cd3999942275cf3776f5df7d
Georgy Chabukiani <georgy.chabukiani@globallogic.com>
2019-03-01 19:10:43 +0200

    gnss: tests for GnssNavTimeUTCParser
    Jira id: ADM-2917.
    
    Change-Id: I615445385e389125b7975f41f6b9ed7bedbc320d
    Signed-off-by: Georgy Chabukiani <georgy.chabukiani@globallogic.com>

---
+++ tests/parsers/nav_time_utc_parser.cpp
@@ -0,0 +1,165 @@
+#define LOG_TAG "GnssHalTesting"
+#include <gtest/gtest.h>
+#include <log/log.h>
+#include <string>
+
+#include "GnssNavTimeUTCParser.h"
+#include "GnssMeasQueue.h"
+
+static const char navTimeUtcDump[] = {
+    0x60, 0xee, 0x5e, 0x0c,
+    0x0a, 0x00, 0x00, 0x00,
+    0x31, 0xde, 0xfb, 0xff,
+    0xe3, 0x07, 0x02, 0x1a,
+    0x09, 0x26, 0x32, 0x37,
+};
+
+class GnssNavTimeUTCParserTest : public GnssNavTimeUTCParser, public ::testing::Test {
+ protected:
+    void SetUp() {}
+};
+
+TEST_F(GnssNavTimeUTCParserTest, createObjDumpInput)
+{
+    GnssNavTimeUTCParser obj(navTimeUtcDump, (uint16_t)sizeof(navTimeUtcDump));
+    (void) obj;
+}
+
+TEST_F(GnssNavTimeUTCParserTest, createObjWithNullPayloadNormalSize)
+{
+    GnssNavTimeUTCParser obj(nullptr, (uint16_t)sizeof(navTimeUtcDump));
+    (void) obj;
+}
+
+TEST_F(GnssNavTimeUTCParserTest, createObjWithDumpInputZeroLen)
+{
+    GnssNavTimeUTCParser obj(navTimeUtcDump, (uint16_t)0);
+    (void) obj;
+}
+
+TEST_F(GnssNavTimeUTCParserTest, createObjWithDumpInputMaxLen)
+{
+    GnssNavTimeUTCParser obj(navTimeUtcDump, (uint16_t)-1);
+    (void) obj;
+}
+
+TEST_F(GnssNavTimeUTCParserTest, createObjWithNullInputZeroLen)
+{
+    GnssNavTimeUTCParser obj(nullptr, (uint16_t)0);
+    (void) obj;
+}
+
+TEST_F(GnssNavTimeUTCParserTest, createObjWithNullInputMaxLen)
+{
+    GnssNavTimeUTCParser obj(nullptr, (uint16_t)-1);
+    (void) obj;
+}
+
+TEST_F(GnssNavTimeUTCParserTest, checkRetrieveNotReadyNullPayloadNormalSize)
+{
+    GnssNavTimeUTCParser obj(nullptr, (uint16_t)sizeof(navTimeUtcDump));
+    IGnssMeasurementCallback::GnssData data;
+    ASSERT_EQ(NotReady, obj.retrieveSvInfo(data));
+}
+
+TEST_F(GnssNavTimeUTCParserTest, checkRetrieveNotReadyWithDumpInputZeroLen)
+{
+    GnssNavTimeUTCParser obj(navTimeUtcDump, (uint16_t)0);
+    IGnssMeasurementCallback::GnssData data;
+    ASSERT_EQ(NotReady, obj.retrieveSvInfo(data));
+}
+
+TEST_F(GnssNavTimeUTCParserTest, checkRetrieveNotReadyWithDumpInputMaxLen)
+{
+    GnssNavTimeUTCParser obj(navTimeUtcDump, (uint16_t)-1);
+    IGnssMeasurementCallback::GnssData data;
+    ASSERT_EQ(NotReady, obj.retrieveSvInfo(data));
+}
+
+TEST_F(GnssNavTimeUTCParserTest, checkRetrieveNotReadyWithNullInputZeroLen)
+{
+    GnssNavTimeUTCParser obj(nullptr, (uint16_t)0);
+    IGnssMeasurementCallback::GnssData data;
+    ASSERT_EQ(NotReady, obj.retrieveSvInfo(data));
+}
+
+TEST_F(GnssNavTimeUTCParserTest, checkRetrieveNotReadyWithNullInputMaxLen)
+{
+    GnssNavTimeUTCParser obj(nullptr, (uint16_t)-1);
+    IGnssMeasurementCallback::GnssData data;
+    ASSERT_EQ(NotReady, obj.retrieveSvInfo(data));
+}
+
+TEST_F(GnssNavTimeUTCParserTest, checkRetrieveNotReadyInvalidWeekFlag)
+{
+    const char navTimeUtcInvalidFlag[] = {
+        0x60, 0xee, 0x5e, 0x0c,
+        0x0a, 0x00, 0x00, 0x00,
+        0x31, 0xde, 0xfb, 0xff,
+        0xe3, 0x07, 0x02, 0x1a,
+        0x09, 0x26, 0x32, 0x35,
+    };
+
+    GnssNavTimeUTCParser obj(navTimeUtcInvalidFlag, (uint16_t)sizeof(navTimeUtcInvalidFlag));
+    IGnssMeasurementCallback::GnssData data;
+    ASSERT_EQ(NotReady, obj.retrieveSvInfo(data));
+}
+
+TEST_F(GnssNavTimeUTCParserTest, checkRetrieveNotReadyInvalidUtcFlag)
+{
+    const char navTimeUtcInvalidFlag[] = {
+        0x60, 0xee, 0x5e, 0x0c,
+        0x0a, 0x00, 0x00, 0x00,
+        0x31, 0xde, 0xfb, 0xff,
+        0xe3, 0x07, 0x02, 0x1a,
+        0x09, 0x26, 0x32, 0x33,
+    };
+
+    GnssNavTimeUTCParser obj(navTimeUtcInvalidFlag, (uint16_t)sizeof(navTimeUtcInvalidFlag));
+    IGnssMeasurementCallback::GnssData data;
+    ASSERT_EQ(NotReady, obj.retrieveSvInfo(data));
+}
+
+TEST_F(GnssNavTimeUTCParserTest, checkRetrieveNotReadyInvalidITowFlag)
+{
+
+    const char navTimeUtcInvalidFlag[] = {
+        0x60, 0xee, 0x5e, 0x0c,
+        0x0a, 0x00, 0x00, 0x00,
+        0x31, 0xde, 0xfb, 0xff,
+        0xe3, 0x07, 0x02, 0x1a,
+        0x09, 0x26, 0x32, 0x36,
+    };
+
+    GnssNavTimeUTCParser obj(navTimeUtcInvalidFlag, (uint16_t)sizeof(navTimeUtcInvalidFlag));
+    IGnssMeasurementCallback::GnssData data;
+    ASSERT_EQ(UTCDone, obj.retrieveSvInfo(data));
+}
+
+TEST_F(GnssNavTimeUTCParserTest, checkRetrieveNotReadyInvalidZeroFlag)
+{
+    const char navTimeUtcInvalidFlag[] = {
+        0x60, 0xee, 0x5e, 0x0c,
+        0x0a, 0x00, 0x00, 0x00,
+        0x31, 0xde, 0xfb, 0xff,
+        0xe3, 0x07, 0x02, 0x1a,
+        0x09, 0x26, 0x32, 0x00,
+    };
+
+    GnssNavTimeUTCParser obj(navTimeUtcInvalidFlag, (uint16_t)sizeof(navTimeUtcInvalidFlag));
+    IGnssMeasurementCallback::GnssData data;
+    ASSERT_EQ(NotReady, obj.retrieveSvInfo(data));
+}
+
+TEST_F(GnssNavTimeUTCParserTest, checkValidNanoTimeAndAcc)
+{
+    const int64_t nanoTime = 1551173929999729201;
+    const double nanoAcc = 10.0;
+
+    GnssNavTimeUTCParser obj(navTimeUtcDump, (uint16_t)sizeof(navTimeUtcDump));
+    IGnssMeasurementCallback::GnssData data;
+    EXPECT_EQ(UTCDone, obj.retrieveSvInfo(data));
+
+    EXPECT_EQ(nanoTime, data.clock.timeNs);
+    EXPECT_EQ(nanoAcc, data.clock.timeUncertaintyNs);
+}
